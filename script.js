b4fdf4ed63e74be2e0a48982fa045def858bf62b
VidishaPoojari <153760645+VidishaPoojari@users.noreply.github.com>
2024-01-06 08:19:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Smooth scrolling for anchor links
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+
+    
+function toggleMenu(){
+        const menu = document.querySelector(".menu-links");
+        const icon = document.querySelector(".hamburger-icon");
+        menu.classList.toggle("open");
+        icon.classList.toggle("open");
+}
+  
